af54f77fbb6e52e04326fd49809287a9686740f8
Rohit Dahale <rohit.dahale@mitaoe.ac.in>
2025-06-29 16:16:36 +0530

    Configure Gradle for JitPack publishing

--- settingslib/build.gradle.kts
+++ settingslib/build.gradle.kts
@@ -3,13 +3,15 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+group = "com.github.rohitdahale" // 🔁 Replace with your GitHub username
+version = "1.0.0" // 🔁 This should match the GitHub release tag (v1.0.0)
+
 android {
     namespace = "com.example.settingslib"
     compileSdk = 33
 
     defaultConfig {
         minSdk = 24
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -23,20 +25,22 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
